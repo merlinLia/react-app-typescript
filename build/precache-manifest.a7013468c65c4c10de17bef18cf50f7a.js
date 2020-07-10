f8a89c50b0a8d3ca6f93fbb59d5a28654a4bae60
LayMui <laymui.toh@taiger.com>
2020-07-10 17:28:23 +0800

    Remove git folder

---
+++ build/precache-manifest.a7013468c65c4c10de17bef18cf50f7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4b0e8dd5c758fb0003b02c26c2ff41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48315d80b2698936ea6c",
+    "url": "/static/css/main.cbd5b20e.chunk.css"
+  },
+  {
+    "revision": "d85a6f104f5398e3682a",
+    "url": "/static/js/2.7349ba77.chunk.js"
+  },
+  {
+    "revision": "48315d80b2698936ea6c",
+    "url": "/static/js/main.01a75253.chunk.js"
+  },
+  {
+    "revision": "201fb28831a6d557384f",
+    "url": "/static/js/runtime-main.624625c9.js"
+  }
+]);
